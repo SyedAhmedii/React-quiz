e647288413c5810e16948bb3777e51be5d5eaee7
Ahmed <syedmuhmmad217@gmail.com>
2024-09-09 23:10:35 +0500

    code completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Quiz from './Components/Quiz'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='bg-purple-600 text-6xl font-bold text-center text-black py-5'>Quiz App</h1>
+      <Quiz />
+    </>
+  )
+}
+
+export default App
